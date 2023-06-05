b0f3cdf8f5773810635e3c9db25d274a29465c0b
ramkrishnaghimire <ramkrishnaghimire8@gmail.com>
2023-06-05 08:37:19 +0545

    second commit

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,6 +1,5 @@
 #include "Graph.hpp"
 #include <iostream>
-using namespace std;
 
 Graph::Graph(bool isDirected) : numV(0), numE(0), directed(isDirected), adjacencyMatrix(nullptr) {}
 
@@ -13,29 +12,24 @@ Graph::~Graph() {
     }
 }
 
-bool Graph::isEmpty()
-{
+bool Graph::isEmpty() {
     return (numV == 0);
 }
 
-bool Graph::isDirected()
-{
+bool Graph::isDirected() {
     return directed;
 }
 
-void Graph::addVertex(int newVertex) 
-{
+void Graph::addVertex(int newVertex) {
     if (newVertex <= 0) {
-        cout<<"Invalid vertex!"<<endl;
+        std::cout << "Invalid vertex!" << std::endl;
     }
     numV++;
 
     bool** newMatrix = new bool*[numV];
-    for (int i = 0; i < numV; i++) 
-    {
+    for (int i = 0; i < numV; i++) {
         newMatrix[i] = new bool[numV];
-        for (int j = 0; j < numV; j++) 
-        {
+        for (int j = 0; j < numV; j++) {
             // Initialize with zeros
             newMatrix[i][j] = 0;   
         }
@@ -54,58 +48,45 @@ void Graph::addVertex(int newVertex)
     adjacencyMatrix = newMatrix;
 }
 
-void Graph::addEdge(int vertex1, int vertex2)
-{
-    if(isEmpty())
-    {
-        cout<<"The graph is empty."<<endl;
+void Graph::addEdge(int vertex1, int vertex2) {
+    if (isEmpty()) {
+        std::cout << "The graph is empty." << std::endl;
         return;
     }
-
-    else if(vertex1 > numV || vertex2 > numV)
-    {
-        if(vertex1 > numV)
-        {
-            cout<<"Vertex "<<vertex1<<"doesn't exist in graph."<<endl;
+    else if (vertex1 > numV || vertex2 > numV) {
+        if (vertex1 > numV) {
+            std::cout << "Vertex " << vertex1 << " doesn't exist in the graph." << std::endl;
         }
-        if(vertex2 > numV)
-        {
-            cout<<"Vertex "<<vertex2<<"doesn't exist in graph."<<endl;
+        if (vertex2 > numV) {
+            std::cout << "Vertex " << vertex2 << " doesn't exist in the graph." << std::endl;
         }
         return;
     }
-
-    else
-    {
+    else {
         adjacencyMatrix[vertex1 - 1][vertex2 - 1] = 1;
-        if(!directed)
-        {
+        if (!directed) {
             adjacencyMatrix[vertex2 - 1][vertex1 - 1] = 1;
         }
-        
     }
     numE++;
 }
 
-int Graph::numVertices(){
+int Graph::numVertices() {
     return numV;
 }
 
-int Graph::numEdges(){
+int Graph::numEdges() {
     return numE;
 }
 
-int Graph::indegree(int vertex)
-{
-    if (isEmpty()) 
-    {
-        cout<<"The graph is empty."<<endl;
+int Graph::indegree(int vertex) {
+    if (isEmpty()) {
+        std::cout << "The graph is empty." << std::endl;
         return 0;
     }
 
-    if(vertex>numV)
-    {
-        cout<<"Invalid vertex!"<<endl;
+    if (vertex > numV) {
+        std::cout << "Invalid vertex!" << std::endl;
         return -1;
     }
 
@@ -119,8 +100,56 @@ int Graph::indegree(int vertex)
     return count;
 }
 
-void Graph::display()
-{
+int Graph::outdegree(int vertex) {
+    if (isEmpty()) {
+        std::cout << "The graph is empty." << std::endl;
+        return 0;
+    }
+
+    if (vertex > numV) {
+        std::cout << "Invalid vertex!" << std::endl;
+        return -1;
+    }
+
+    int count = 0;
+    for (int j = 0; j < numV; j++) {
+        if (adjacencyMatrix[vertex - 1][j]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int Graph::degree(int vertex) {
+    if (isEmpty()) {
+        std::cout << "The graph is empty." << std::endl;
+        return 0;
+    }
+
+    if (vertex > numV) {
+        std::cout << "Invalid vertex!" << std::endl;
+        return -1;
+    }
+
+    return indegree(vertex) + outdegree(vertex);
+}
+
+bool Graph::neighbour(int vertex1, int vertex2) {
+    if (isEmpty()) {
+        std::cout << "The graph is empty." << std::endl;
+        return false;
+    }
+
+    if (vertex1 > numV || vertex2 > numV) {
+        std::cout << "Invalid vertices!" << std::endl;
+        return false;
+    }
+
+    return adjacencyMatrix[vertex1 - 1][vertex2 - 1] || adjacencyMatrix[vertex2 - 1][vertex1 - 1];
+}
+
+void Graph::display() {
     for (int i = 0; i < numV; i++) {
         for (int j = 0; j < numV; j++) {
             std::cout << adjacencyMatrix[i][j] << " ";
